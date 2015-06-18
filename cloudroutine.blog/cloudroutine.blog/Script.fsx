f7f8d769547cf69c46390f6b98ec1f6f15d2e321
Jared Hester <jared.hester@gmail.com>
2015-06-18 09:01:02 -0400

    Gembundle and FAKE

--- cloudroutine.blog/cloudroutine.blog/Script.fsx
+++ cloudroutine.blog/cloudroutine.blog/Script.fsx
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
+#load @"..\..\packages\FSharp.Formatting\lib\net40\FSharp.CodeFormat.dll"
+
 open cloudroutine.blog
 
 // Define your library scripting code here
